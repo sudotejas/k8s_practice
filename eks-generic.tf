e53bde4494e0bbf6a06435dca0b758c0576ede43
ytejas <yrajatejas@gmail.com>
2021-05-20 16:31:35 -0400

    Marked manage_aws_auth as false

--- eks-generic.tf
+++ eks-generic.tf
@@ -87,6 +87,7 @@ module "eks-cluster-dev" {
   cluster_name    = local.cluster_name
   cluster_version = "1.20"
   subnets         = module.eks-vpc-dev.private_subnets
+  manage_aws_auth  = false   
 
   tags = {
     Environment = "test"
@@ -115,13 +116,4 @@ module "eks-cluster-dev" {
 
   worker_additional_security_group_ids = [aws_security_group.all_worker_mgmt.id]
 
-  map_users = [
-    {
-      userarn  = "arn:aws:iam::423321982572:user/eks-admin"
-      username = "eks-admin"
-      groups   = ["system_masters"]
-    }
-  ]
-
-  map_accounts = ["423321982572", ]
 }
