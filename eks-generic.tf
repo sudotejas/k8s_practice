31fefa5b5c5ea5f34040423e400aa032d2c3e5e6
Tejas <13282079+sudotejas@users.noreply.github.com>
2021-05-20 09:45:03 -0400

    AWS VPC and EKS - Terraform
    Error on Terraform apply:
    
    
    Error: Invalid count argument
    
      on .terraform/modules/eks-cluster-dev/data.tf line 17, in data "aws_ami" "eks_worker":
      17:   count = local.worker_has_linux_ami ? 1 : 0
    
    The "count" value depends on resource attributes that cannot be determined
    until apply, so Terraform cannot predict how many instances will be created.
    To work around this, use the -target argument to first apply only the
    resources that the count depends on.
    
    
    Error: Invalid count argument
    
      on .terraform/modules/eks-cluster-dev/data.tf line 30, in data "aws_ami" "eks_worker_windows":
      30:   count = local.worker_has_windows_ami ? 1 : 0
    
    The "count" value depends on resource attributes that cannot be determined
    until apply, so Terraform cannot predict how many instances will be created.
    To work around this, use the -target argument to first apply only the
    resources that the count depends on.

---
+++ eks-generic.tf
@@ -0,0 +1,127 @@
+data "aws_availability_zones" "available" {
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+locals {
+  cluster_name = "eks-dev-${random_string.suffix.result}"
+}
+
+resource "aws_security_group" "all_worker_mgmt" {
+  name_prefix = "all_worker_management"
+  vpc_id      = module.eks-vpc-dev.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+}
+
+resource "aws_security_group" "worker_group_mgmt_one" {
+  name_prefix = "worker_group_mgmt_one"
+  vpc_id      = module.eks-vpc-dev.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+    ]
+  }
+}
+
+resource "aws_security_group" "worker_group_mgmt_two" {
+  name_prefix = "worker_group_mgmt_two"
+  vpc_id      = module.eks-vpc-dev.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "192.168.0.0/16",
+    ]
+  }
+}
+
+
+module "eks-vpc-dev" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0.0"
+
+  name                 = "eks-vpc-dev"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+module "eks-cluster-dev" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.20"
+  subnets         = module.eks-vpc-dev.private_subnets
+
+  tags = {
+    Environment = "test"
+    GithubRepo  = "terraform-aws-eks"
+    GithubOrg   = "terraform-aws-modules"
+  }
+
+  vpc_id = module.eks-vpc-dev.vpc_id
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t3.small"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    },
+    {
+      name                          = "worker-group-2"
+      instance_type                 = "t3.medium"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 1
+    },
+  ]
+
+  worker_additional_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+
+  map_users = [
+    {
+      userarn  = "arn:aws:iam::423321982572:user/eks-admin"
+      username = "eks-admin"
+      groups   = ["system_masters"]
+    }
+  ]
+
+  map_accounts = ["423321982572", ]
+}
